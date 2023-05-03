003c21392c694667823d0706ca817531a1bb399d
Evelyne Kayisinga <104898081+Loner010@users.noreply.github.com>
2023-05-03 23:47:22 +1400

    Add files via upload

---
+++ IWA14/Challenge 1/script.js
@@ -0,0 +1,14 @@
+const name = 'John';
+const age = 35;
+const hobby = 'Coding';
+
+const logTwice = (parameter) => {
+  console.log(parameter);
+  console.log(parameter);
+}
+
+function myHobby () {
+  logTwice(`Hello, ${name} (${age}). I love ${hobby}!`);
+}
+
+myHobby();
